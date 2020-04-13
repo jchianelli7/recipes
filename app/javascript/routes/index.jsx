6505e9d28ba3f55ea2549e845846cd80d192fc5e
Josh Chianelli <joshchianelli@gmail.com>
2020-04-13 13:46:08 -0500

    [FEATURE] view all and single recipe

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "../components/home";
-
+import Recipes from '../components/recipes'
+import Recipe from '../components/recipie'
 // To add more pages, declare a route in this file and match it to the component you want to render for that page.
 
 export default (
     <Router>
         <Switch>
             <Route path="/" exact component={Home}/>
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipe/:id" exact component={Recipe} />
         </Switch>
     </Router>
 );
